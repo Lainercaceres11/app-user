6bb9f12a3bef09253501f243a723b8bce2e8c551
Lainer <lainercaceres12@gmail.com>
2023-08-15 22:04:42 -0500

    feat: config app

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
-import { printHello } from '#Lib/hello.js';
+import connectDB from './config/db.js';
+import httpServer from './config/http.js';
+import dotenv from 'dotenv';
 
-printHello();
+dotenv.config();
+
+const PORT = 3000;
+const URL_DB =
+    'mongodb+srv://lainercaceres:YbdtIAVUbRWLwlSw@node-course.8fllnj7.mongodb.net/';
+
+const bootstrap = async () => {
+    await connectDB(URL_DB);
+    httpServer.listen(PORT, () => {
+        console.log(`Server run in port ${PORT}  `);
+    });
+};
+
+bootstrap();
